55b9c7cd62c6292c24c383a9835ec47d0c62418d
jhuynh125087 <jhuynh125@student.hbuhsd.edu>
2025-10-07 18:21:41 +0000

    Question 2

--- Main.java
+++ Main.java
@@ -5,12 +5,27 @@ public class Main
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
-    /* ============= BEGIN SOLUTION ================================ */
+  
+    double target = 12.345;
+    System.out.println ("Enter a number");
+    double input = scan.nextDouble ();
 
+    if (input == target)
+    {
+      System.out.println ("YES!");
+    }
+    else
+    {
+      System.out.println("NO"); 
+    }
 
+    int targetNum = 48;
+    System.out.println ("Enter a number: ");
+    int input2 = scan.nextInt ();
 
+    if (input2 == targetNUM)
 
-    /* ============= END SOLUTION ================================== */
+    
     scan.close();
   }
 }
